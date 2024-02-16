aeac173dff0f29dc7224d737e1af0a6ec26f8829
felix1251 <domhack123@gmail.com>
2024-02-17 04:44:29 +0800

    molecules LandingInfo paragraph responsive

--- src/components/molecules/LandingInfo.tsx
+++ src/components/molecules/LandingInfo.tsx
@@ -8,7 +8,7 @@ const LandingInfo: React.FunctionComponent = () => {
 
   return (
     <div className="flex flex-col gap-10">
-      <p className="text-white text-[1.5rem] font-medium w-[900px]">
+      <p className="text-white text-[1.5rem] font-medium w-full lg:w-[900px]">
         Hi <span className="text-3xl">👋</span>, welcome to the{" "}
         <span className="font-bold bg-gradient-to-r from-primary to-primary-light text-transparent bg-clip-text">
           Weather Forecast Web Application
