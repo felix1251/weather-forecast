8afe0468c64fc98a816a4d6930f172ceda2ead80
felix1251 <domhack123@gmail.com>
2024-02-17 04:26:26 +0800

    page not found style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NotFound: React.FunctionComponent = () => {
-  return <div>page NotFound</div>;
+  return (
+    <div className="grid place-items-center h-full w-full">
+      <h2 className="text-red-500 text-4xl font-semibold">
+        404 Page Not found
+      </h2>
+    </div>
+  );
 };
 
 export default NotFound;
