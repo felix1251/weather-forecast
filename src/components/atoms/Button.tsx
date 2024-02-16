0a62225d3c3cf4f27bec9a3eb425476ea787b481
felix1251 <domhack123@gmail.com>
2024-02-16 16:58:46 +0800

    atom button variant secondary-light

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "secondary-light";
+  size?: "sm" | "md" | "lg";
 }
 
 const Button: React.FunctionComponent<IButtonProps> = ({
   children,
   variant = "primary",
+  size = "md",
   ...props
 }: IButtonProps) => {
   return (
     <button
-      className={`flex items-center text-lg gap-2 px-6 py-2.5 font-medium rounded-md w-fit ${conditionalVariant(
+      className={`flex items-center gap-2 font-medium rounded-md w-fit ${conditionalVariant(
         variant
-      )}`}
+      )} ${conditionalSize(size)}`}
       {...props}
     >
       {children}
@@ -34,6 +36,23 @@ const conditionalVariant = (variant: string): string => {
       return defaultVariant;
     case "secondary":
       return "bg-secondary text-white hover:shadow-md hover:bg-secondary-light transition-colors duration-200 ease-in-out disabled:bg-primary-light";
+    case "secondary-light":
+      return "bg-secondary-light text-white hover:scale[1.02]";
+    default:
+      return defaultVariant;
+  }
+};
+
+const conditionalSize = (size: string): string => {
+  const defaultVariant = "px-6 py-2.5 text-lg";
+
+  switch (size) {
+    case "md":
+      return defaultVariant;
+    case "sm":
+      return "px-5 py-2.5 text-base";
+    case "lg":
+      return "px-6 py-3 text-lg";
     default:
       return defaultVariant;
   }
--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,17 +1,23 @@
+import { Button } from "@/atoms";
 import React from "react";
 import { IoCloud } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const Navbar: React.FunctionComponent = () => {
   return (
     <header className="w-full h-20 flex items-center bg-secondary shadow-md">
-      <div className="margin-max-screen">
+      <div className="margin-max-screen flex justify-between">
         <Link to="/" className="flex gap-4 items-center">
           <IoCloud className="text-primary text-4xl" />
           <h2 className="text-[1.7rem] bg-gradient-to-r from-primary to-primary-light text-transparent bg-clip-text font-bold">
             Weather Forecast
           </h2>
         </Link>
+        <Button variant="secondary-light" size="sm">
+          <MdLogout className="text-xl" />
+          Logout
+        </Button>
       </div>
     </header>
   );
