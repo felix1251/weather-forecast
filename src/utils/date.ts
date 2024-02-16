7033509e14f1a31e21e396947cad3f855185b3d2
felix1251 <domhack123@gmail.com>
2024-02-17 04:19:09 +0800

    getFormattedDate use const for varibles (fix lint)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,7 @@
 export function getFormattedDate(date: Date): string {
-  let year = date.getFullYear();
-  let month = (1 + date.getMonth()).toString().padStart(2, "0");
-  let day = date.getDate().toString().padStart(2, "0");
+  const year = date.getFullYear();
+  const month = (1 + date.getMonth()).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
   return month + "/" + day + "/" + year;
 }
