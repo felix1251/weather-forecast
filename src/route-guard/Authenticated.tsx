4ec476a6895c5aeda633588f2d430cdae4e43afa
felix1251 <domhack123@gmail.com>
2024-02-17 03:23:39 +0800

    uppercase funtion name Authenticated route guard

--- src/route-guard/Authenticated.tsx
+++ src/route-guard/Authenticated.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-const authenticated: React.FunctionComponent = () => {
+const Authenticated: React.FunctionComponent = () => {
   const { isAuthenticated } = useAuth0();
 
   if (isAuthenticated === undefined) {
@@ -12,4 +12,4 @@ const authenticated: React.FunctionComponent = () => {
   return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 };
 
-export default authenticated;
+export default Authenticated;
