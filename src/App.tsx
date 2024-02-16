7adb7ef1c856045fc6d82ae56be0974297e2155d
felix1251 <domhack123@gmail.com>
2024-02-16 15:18:03 +0800

    setup RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import router from "@/router";
+import { RouterProvider } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
